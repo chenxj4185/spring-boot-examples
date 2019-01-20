9ab0dcb4436a69e607a4abd4b5c8d62ceaea2759
陈晓佳 <chenxj@bjrrtx.com>
2019-01-20 14:29:29 +0800

    添加注释, 不同的annotation效果不同

--- spring-boot-helloWorld/src/main/java/com/neo/controller/HelloWorldController.java
+++ spring-boot-helloWorld/src/main/java/com/neo/controller/HelloWorldController.java
@@ -2,8 +2,15 @@ package com.neo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+/**
+  * ClassName: HelloWorldController<br>
+  * Description:  <br>
+  * Company: com.cxj
+  * @author chenxj
+  * @date  2019/1/20-14:15
+  */
+@RestController //必须是这个annotation 才有效, controller注解没有效果.
 
-@RestController
 public class HelloWorldController {
 	
     @RequestMapping("/hello")
